c7017eb716a52ae69b8603867814a4b318a1b390
LuisFernandoMolina <luisfernandomolinay@outlook.com>
2017-10-26 16:41:25 -0500

    Tarea 2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -10,11 +10,21 @@ export class AppComponent {
   name ='Fernando';
   lastname= 'Molina';
   colores=['negro','amarillo','rojo','verde'];
+  mostrar=false;
+  btn_msj="Mostrar";
 
   addcolor(color){
     this.colores.push(color.value);
     color.value="";
     return false;
+  }
+  mostrarcolores(){
+    this.mostrar=!this.mostrar;
+    if(this.btn_msj=="Ocultar"){
+      this.btn_msj="Mostrar";
+    }else{
+      this.btn_msj="Ocultar";
+    }
   }
 
 }
